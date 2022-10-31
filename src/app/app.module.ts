4448b7243e7bef86af4cc067d06987f0a31dff9e
Wizzartoz <misha.maznichko@ukr.net>
2022-10-31 23:00:44 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import { HomePageLayoutComponent } from './home-page-layout/home-page-layout.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomePageComponent,
+    HomePageLayoutComponent,
+    RegisterComponent,
+    AboutComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
+  exports: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
